813e65789dc44320436a005bdfd05e63e6912aff
smctrl <stjj@kokosema.net>
2021-03-08 03:01:28 +0900

    CORSリクエストに修正

--- src/search/search.ts
+++ src/search/search.ts
@@ -3,7 +3,7 @@ import {computedFrom} from 'aurelia-framework';
 export class Search {
   public message: string = '接続されていません'
   constructor() {
-    fetch('https://localhost/connectivity')
+    fetch('https://localhost/connectivity', { mode: 'cors' })
       .then(response => response.json())
       .then(() => this.message = '接続されています')
       .catch(reason => this.message = JSON.stringify(reason))
