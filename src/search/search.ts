7667c4031463df86d192b8a5acce864953b8fae1
smctrl <stjj@kokosema.net>
2021-03-08 01:59:55 +0900

    接続テスト先を修正

--- src/search/search.ts
+++ src/search/search.ts
@@ -3,7 +3,7 @@ import {computedFrom} from 'aurelia-framework';
 export class Search {
   public message: string = '接続されていません'
   constructor() {
-    fetch('https://localhost:55000')
+    fetch('https://localhost/connectivity')
       .then(response => response.json())
       .then(() => this.message = '接続されています')
       .catch(reason => this.message = JSON.stringify(reason))
